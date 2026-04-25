f2341d6c02f98859fd9fcb339f393f84ed5a400a
Cherry Lin <pdt8ye@virginia.edu>
2026-04-24 22:57:00 -0400

    Bare UI with search, clickable create event form

--- showUp/dashboard/views.py
+++ showUp/dashboard/views.py
@@ -1,8 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+
+# import forms
+from .forms import EventPostForm
+from .models import EventPost
+
 
 # Create your views here.
-def index(request):
+def dashboard_home(request):
     # return HttpResponse("Setting up our initial Django app.")
-    return render(request, "dashboard/home.html")
+
+    # Create an empty instance of the form
+    form = EventPostForm() 
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, "dashboard/home.html", context)
+
+
+def create_event_post(request):
+    if request.method == 'POST':
+        # Handle deletion
+        # if 'delete_post_id' in request.POST:
+        #     post_id = request.POST.get('delete_post_id')
+        #     CarpoolPost.objects.filter(id=post_id, author=request.user).delete()
+        #     messages.success(request, 'Post deleted successfully.')
+        #     return redirect('driver dashboard')
+
+        # Handle creation
+        form = EventPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # post.author = request.user
+            post.save()
+            messages.success(request, 'Event post created successfully!')
+            return redirect('dashboard_home')
+        
+    return redirect('dashboard_home')
+
+
 
--- showUp/showUp_project/settings.py
+++ showUp/showUp_project/settings.py
@@ -125,10 +125,12 @@ else:
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+# # Custom User Model
+# AUTH_USER_MODEL = 'dashboard.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
